e670563ab881caf3f4afde584b4ccc7e30982bd0
nss213 <nss213@users.noreply.github.com>
2023-06-19 21:39:17 -0700

    fixing up styling on play/pause button. using tailwind icons

--- src/widgets/audio-player.jsx
+++ src/widgets/audio-player.jsx
@@ -27,11 +27,12 @@ var IS_URL = /^https?:\/\//;
 
 var buttonStyle = {
     backgroundColor: ltbBlue,
-    padding: "10px",
+    padding: "7px",
     color: "white",
     border: "none",
     borderRadius: "3px",
-    fontSize: "20px",
+    height: "2.4rem",
+    width: "2.4rem",
 }
 
 /**
@@ -86,6 +87,22 @@ var Audio = createReactClass({
         })
     },
 
+    playIcon: function() {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style={{width:"1.5rem", height:"1.5rem"}}>
+                <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
+            </svg>
+        )
+    },
+    
+    pauseIcon: function() {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style={{width:"1.5rem", height:"1.5rem"}}>
+                <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z" clipRule="evenodd" />
+            </svg>
+        )
+    },
+
     render: function() {
         var location = this.props.location;
         if (!location) {
@@ -104,7 +121,7 @@ var Audio = createReactClass({
             <div className="perseus-audio-widget">
                 <audio ref={this.audioplayer} src={url} preload="auto" />
                 <button aria-label={this.state.play? "pause" : "play"} onClick={this.togglePlayPause} style={buttonStyle}>
-                    {this.state.play? "▮▮" : "▶"}
+                    {this.state.play? this.pauseIcon() : this.playIcon()}
                 </button>
             </div>
         );
