7d0dc8b43e4a6b9136cd75e59488124802a81a61
Neeraj Kapoor <neeraj.kapoor@learntobe.org>
2025-08-26 19:19:19 -0700

    make image widget available for all urls

--- editor-perseus.js
+++ editor-perseus.js
@@ -2,7 +2,7 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var perseus = require('./perseus-4f8e50b0.js');
+var perseus = require('./perseus-f08e857e.js');
 var aphrodite = require('aphrodite');
 var _react = require('react');
 var _underscore = require('underscore');
--- perseus.js
+++ perseus.js
@@ -2,7 +2,7 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var perseus = require('./perseus-4f8e50b0.js');
+var perseus = require('./perseus-f08e857e.js');
 require('react-addons-pure-render-mixin');
 require('jquery');
 require('react');
