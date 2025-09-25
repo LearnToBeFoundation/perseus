8cd6bc31acbc9a425170f970b4b87b2c8af9befc
Neeraj Kapoor <neeraj.kapoor@learntobe.org>
2025-09-25 13:11:43 -0700

    update (#17)

--- editor-perseus.js
+++ editor-perseus.js
@@ -2,7 +2,7 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var perseus = require('./perseus-000c84de.js');
+var perseus = require('./perseus-5439edfd.js');
 var aphrodite = require('aphrodite');
 var _react = require('react');
 var _underscore = require('underscore');
